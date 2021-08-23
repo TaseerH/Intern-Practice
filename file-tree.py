72eba81544109849cd764efcf4ddf3edcab80135
TaseerH <73301847+TaseerH@users.noreply.github.com>
2021-08-23 14:29:31 +0500

    working but desired tree does not form

--- file-tree.py
+++ file-tree.py
@@ -1,18 +1,36 @@
 import os
 
 
-def make_tree (user_path):
+
+def make_tree (user_path, i):
+    if i == 1 :
+        print ("|---->"+os.path.basename(os.path.normpath(user_path)))
+    elif i == 2 :
+        print ("    |---->"+os.path.basename(os.path.normpath(user_path)))
+    elif i == 3 :
+        print ("        |---->"+os.path.basename(os.path.normpath(user_path)))
+    elif i > 3 :
+        print ("            |---->"+os.path.basename(os.path.normpath(user_path)))
+    else:
+        print (os.path.basename(os.path.normpath(user_path)))
+        print ("|")
     for item in os.listdir(user_path):
         item = os.path.join(user_path, item)
         if os.path.isfile(item):
-            print("     "+os.path.basename(os.path.normpath(item)) + " is a file")
+            if i == 1:
+                print("     |->"+os.path.basename(os.path.normpath(item)) + " is a file")
+            elif i == 2 :
+                print ("        |---->"+os.path.basename(os.path.normpath(item))+ " is a file")
+            elif i > 2 :
+                print ("            |---->"+os.path.basename(os.path.normpath(item))+ " is a file")
+            else:
+                print("|->"+os.path.basename(os.path.normpath(item)) + " is a file")
         elif os.path.isdir(item):
-            print(os.path.basename(os.path.normpath(item)) + " is a dir")
-            make_tree(item)
+            i = i+1
+            make_tree(item, i)
         else:
             print("Unknown!")
 
 user_path = input ("Enter Path for Tree:" )
-
-
-make_tree(user_path)
+i = 0
+make_tree(user_path, i)
