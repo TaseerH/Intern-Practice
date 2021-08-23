2aa3cc6e230974364cae6822e20f903b4e4adbd7
TaseerH <73301847+TaseerH@users.noreply.github.com>
2021-08-23 12:44:54 +0500

    made indents for clarity

--- file-tree.py
+++ file-tree.py
@@ -4,8 +4,8 @@ import os
 def make_tree (user_path):
     for (root,dirs,files) in os.walk(user_path, topdown= True):
         print (os.path.basename(os.path.normpath(root)))
-        print (dirs)
-        print (files)
+        print ("    {}".format(dirs))
+        print ("        {}".format(files))
 
 
 
